f56d882d0883eacb79442542b31d62e4c2a796d9
tess61 <111118596+tess61@users.noreply.github.com>
2023-04-25 18:29:08 +0300

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python3
-""" lists all states and individual state """
-from models.state import State
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
+
 from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states(id=None):
+    '''The states page.'''
+    states = None
+    state = None
+    all_states = list(storage.all(State).values())
+    case = 404
+    if id is not None:
+        res = list(filter(lambda x: x.id == id, all_states))
+        if len(res) > 0:
+            state = res[0]
+            state.cities.sort(key=lambda x: x.name)
+            case = 2
+    else:
+        states = all_states
+        for state in states:
+            state.cities.sort(key=lambda x: x.name)
+        states.sort(key=lambda x: x.name)
+        case = 1
+    ctxt = {
+        'states': states,
+        'state': state,
+        'case': case
+    }
+    return render_template('9-states.html', **ctxt)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """reloads storage"""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def states():
-    """list of states"""
-    states = storage.all(State).values()
-    return render_template('9-states.html', states=states)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def state(id):
-    """individual state"""
-    key = "State." + id
-    state = storage.all(State).get(key)
-    return render_template('9-states.html', state=state)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
